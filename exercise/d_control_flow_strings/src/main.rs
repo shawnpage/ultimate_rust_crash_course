1b7279d0d0c5acbbebc13f735f8cc5db4b73808f
Shawn Page <shawn.page@gmail.com>
2025-01-04 19:17:33 -0500

    Code for exercises d ane e.

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -11,19 +11,28 @@ fn main() {
     //
     //     vec!["apple".to_string(), "banana".to_string()]
     let args: Vec<String> = std::env::args().skip(1).collect();
+    //let args: Vec<String> = std::env::args().collect();
 
     // This consumes the `args` vector to iterate through each String
     for arg in args {
+        println!("Processing argument: {}", arg);
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
-
+        // You should write the `sum`, `double`, and `count` functions below.
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        }
+    }
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
-    }
 }
 
 fn sum() {
@@ -31,8 +40,9 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-
-
+    for i in 7..=23 {
+        sum += i;
+    }
     println!("The sum is {}", sum);
 }
 
@@ -42,7 +52,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
+    while x <= 500 {
+        x *= 2;
+        count += 1;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -51,8 +64,17 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    let mut count: i32 = 0;
+    loop {
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        println!(); // This will output just a newline at the end for cleanliness.
+        if count == 7  {
+            break;
+        }
+        count += 1;
+    }
 
 
-    println!(); // This will output just a newline at the end for cleanliness.
+
+
 }
